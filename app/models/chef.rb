4e8699a7dcdae886c0efbff9060d324d94092da9
Gleb B <fost_bg@mail.ru>
2015-08-12 06:28:49 +0300

    create chefs and recipes assocoations and test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,17 @@
 class Chef < ActiveRecord::Base
+
+  has_many :recipes
+
+  #перед сохранением приведем все емэйлы к маленьким буквам
+  before_save {self.email = email.downcase}
+
+  validates :chefname, presence: true, length: {minimum:3, maximum: 40}
+
+  #проверка на валидный емэйил
+  VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false},
+            format: { with: VALID_EMAIL_REGEX }
+
+
 end
--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ChefTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @chef = Chef.new(chefname: "Pedro Hulitos",email: "pedropedro@pedro.com")
+  end
+
+  test "chef should be valid" do
+    assert @chef.valid?
+  end
+
+  test "chefname must be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "chefname must be enought long" do
+    @chef.chefname="a"*2
+    assert_not @chef.valid?
+  end
+
+  test "chefname must be not longer then" do
+    @chef.chefname="a"*41
+    assert_not @chef.valid?
+  end
+
+  test "email must be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "email must be within bounds" do
+    @chef.email = "a"*101+"@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "email must be uniq" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not dup_chef.valid?
+  end
+
+  test "email validations should accept valid addresses" do
+    valid_addresses = %w[user@eee.com R_TDD-DS@eee.helllo.org user@example.com fist.last@eeem.au fuck@wad.ru]
+    valid_addresses.each do |va|
+      @chef.email = va
+      assert @chef.valid?, '#{va.inspect} should be valid'
+    end
+  end
+
+  test "email validations should reject invalid addresses" do
+    invalid_addresses = %w[usereee.com R_TDD-DS@eee..org user@example. com fist.lasteeem.au fuck@wa_d.ru]
+    invalid_addresses.each do |ia|
+      @chef.email = ia
+      assert_not @chef.valid?, '#{ia.inspect} should be invalid'
+    end
+  end
 end
